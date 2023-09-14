09bbec2edb1155b8950f259b2a9167fb5249b9ed
Yi-Hsuan Chen <yihsuan09193029@gmail.com>
2023-09-14 15:28:38 -0400

    first commit

---
+++ findLs.m
@@ -0,0 +1,95 @@
+function [L1s,Les,Sks,slacks] = findLs(V,rhopp,ts,fpoly,options,ui,intvs,t,x,k)
+% 
+% Input:
+%   V       -- updated Lyapunov fcn (V0+x'*Phi*x) in 1-by-(N-1) cell with msspoly
+%   rhopp   -- updated piecewise polynomial rho 
+%   ts      -- time samples
+%   fpoly   -- fpoly(t,x,u) Taylor expanded original system (not CL) 
+%   Phi     -- Phi expressed in 1-by-(N-1) cell with n-by-n matrices 
+%   options -- optimization options structure
+%   Vtraj0  -- structure including V0, V0k(dV0/dt), S0s expressed in 1-by-(N-1) cells with msspoly
+% % %   rho0    -- struture including rho and rhodot expressed in 1-by-(N-1) cells with msspoly
+%   ui      -- tvlqr CL control input expressed in 1-by-(N-1) cells with msspoly
+%   intvs   -- time intervals expressed in 1-by-(N-1) cell
+%   t       -- time expressed in 1-by-1 msspoly 
+%   x       -- state expressed in n-by-1 msspoly
+%   k       -- number of time segments
+% Output:
+%   L1      -- Lagrange multipliers in (N-1)-by-1 cell with msspoly
+%   Le      -- Lagrange multipliers in (N-1)-by-1 cell with msspoly
+%   (slacks)-- slack (double) variables for debugging
+% Edited by Yi-Hsuan Chen 08/30/2023
+
+%%=========================== Initialization ===========================
+N       = length(ts);
+
+rhoi    = pp2msspoly(t,rhopp);
+Vs      = repmat(V{1},N-1,k);
+Vdots   = repmat(V{1},N-1,k);
+rhos    = repmat(V{1},N-1,k);
+rhodots = repmat(V{1},N-1,k);
+
+for i = 1:N-1
+    tt          = linspace(intvs{i}(1),intvs{i}(2),k);
+    tts(i,:)    = tt;
+    Vs(i,:)     = msubs(V{i},t,tt);
+    xdoti       = fpoly(t,x,ui{i});
+    Vdotsi      = diff(V{i},x)*xdoti + diff(V{i},t);
+    Vdots(i,:)  = msubs(Vdotsi,t,tt);
+    rhos(i,:)   = msubs(rhoi(:,i),t,tt);
+    rhodots(i,:)= msubs(diff(rhoi(:,i),t),t,tt);
+end
+
+M       = length(Vs(:));
+L1s     = cell(1,M);
+Les     = cell(1,M);
+Sks     = cell(1,M);
+slacks  = zeros(1,M);
+disp('Step 1: Searching for multipliers...')
+
+for i = 1:M
+    % Initialize SOS program
+    prog        = spotsosprog;
+    prog        = prog.withIndeterminate(x);
+    
+    % clean stuff
+    Vi          = clean(Vs(i),options.clean_tol);
+    Vdoti       = clean(Vdots(i),options.clean_tol);  
+    
+    rhoi        = rhos(i);
+    rhodoti     = rhodots(i);
+    
+    % Create a PSD matrix Sk
+    [prog,Sk]   = prog.newPSD(options.xdim);
+    
+    % create multipliers and slack variable
+    [prog,L1]   = prog.newFreePoly(monomials(x,0:options.degL1));
+%     [prog,slack]= prog.newPos(1);
+%     constr1     = -slack*(x'*x)^4 - Vdoti + rhodoti + L1*(Vi-rhoi);
+    constr1     = -Vdoti + rhodoti + L1*(Vi-rhoi);
+    prog        = prog.withSOS(constr1);
+    
+    [prog,Le]   = prog.newFreePoly(monomials(x,0:options.degLe));
+    constr2     = 1-x'*Sk*x-Le*(rhoi-Vi);
+    prog        = prog.withSOS(constr2);
+    
+    % solve problem
+    options_spot            = spot_sdp_default_options();
+    options_spot.verbose    = 0;
+    
+    %%... check the correctness of maxdet function
+    [prog,obj]              = maxdet(prog,Sk);
+%     costfun                 = -slack;
+    costfun                 = obj;
+    sol                     = prog.minimize(costfun,@spot_mosek,options_spot);
+    
+    if ~strcmp(sol.status,'STATUS_PRIMAL_AND_DUAL_FEASIBLE')
+        disp('Primal infeasible')
+    end
+    L1s{i}      = sol.eval(L1);
+    Les{i}      = sol.eval(Le);
+    Sks{i}      = double(sol.eval(Sk));
+%     slacks(i)   = double(sol.eval(slack));
+end
+
+end
